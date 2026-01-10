411a25b2a45891abb92e5f8929923b524b9c88db
Tr0ya7 <DevLuizHBarros@proton.me>
2026-01-09 21:44:53 -0300

    dashboard rh mockado

--- components/DataMetrics/index.tsx
+++ components/DataMetrics/index.tsx
@@ -1,5 +1,7 @@
+import DataChart from "../DataChart"
+
 const DataMetrics = ({ department }: { department: string }) => (
-    <section className="border border-default-border-color w-full"><h1 className="section-title">{ department }</h1></section>
+    <section className="border border-default-border-color w-full rounded-md px-5.5 py-4"><h1 className="section-title">Métricas dos { department }</h1><DataChart /></section>
 )
 
 export default DataMetrics
