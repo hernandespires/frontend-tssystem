f6e01be361a34dd26963de505f8c60b7d875610e
Hernandes <hernandes.pires10@gmail.com>
2026-01-14 21:54:32 -0300

    consertando foto de perfil e logo.v2

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,43 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "standalone",
+  
+//   // ADICIONE ESTES BLOCOS:
+//   typescript: {
+//     // !! ATENÇÃO !!
+//     // Perigo: Ignora erros de tipagem no build para produção.
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     // Ignora erros de linter durante o build
+//     ignoreDuringBuilds: true,
+//   },
+  
+//   images: {
+//     remotePatterns: [{ protocol: "https", hostname: "*.googleusercontent.com" }]
+//   }
+// };
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  
-  // ADICIONE ESTES BLOCOS:
   typescript: {
-    // !! ATENÇÃO !!
-    // Perigo: Ignora erros de tipagem no build para produção.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora erros de linter durante o build
     ignoreDuringBuilds: true,
   },
-  
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "*.googleusercontent.com" }]
+    // ALTERAÇÃO AQUI: Usar curinga (*) para aceitar lh3, lh4, lh5...
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      }
+    ]
   }
 };
 
