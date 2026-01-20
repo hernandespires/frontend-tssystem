05a38d196f50ffe0fcfedda39a29cd9194a2bb3a
hernandespires <hernandespires@trajetoriadosucesso.com>
2026-01-20 17:45:48 -0300

    processos burocraticos

---
+++ app/(main)/rh/processos-burocraticos/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import Button from "@/components/Button"
+import DataMetrics from "@/components/DataMetrics"
+import DataTable from "@/components/DataTable"
+import { useLogin } from "@/contexts/LoginContext"
+// import { CreateEmployeeContext } from "@/contexts/rh/CreateEmployeeContext" // Removido se não for usar agora para evitar erros
+import { redirect } from "next/navigation"
+import { useContext } from "react"
+import { BsClipboardData } from "react-icons/bs"
+import { FaPlus } from "react-icons/fa"
+import { IoPersonAddOutline } from "react-icons/io5"
+import { LuScanFace } from "react-icons/lu"
+import { MdOutlinePersonSearch, MdPeopleOutline } from "react-icons/md"
+import { PiTreeStructure } from "react-icons/pi"
+import { VscSync } from "react-icons/vsc"
+
+const Rh = () => {
+    // const { employeeInformations } = useContext(CreateEmployeeContext) // Comentado por enquanto
+
+    const { user } = useLogin()
+    // if (!user) redirect("/login") // Comentado para você conseguir testar sem backend rodando
+
+    return (
+        <main className="flex flex-col gap-6">
+            <div className="flex gap-5 items-end justify-between">
+                <DataTable filter="Colaboradores" />
+                <div className="flex flex-wrap gap-6 max-w-150">
+                    <Button isFulled onClick={"/"} icon={<BsClipboardData size={36} color="black" />} text="Relatório Mensal" />
+                    <Button isFulled onClick={"/"} icon={<LuScanFace size={36} color="black" />} text="Pontos" />
+                    <Button isFulled onClick={"/"} icon={<MdOutlinePersonSearch size={36} color="black" />} text="Banco de currículos" />
+                    <Button onClick={"/rh/cadastro-colaborador"} icon={<IoPersonAddOutline size={36} color="white" />} text="Cadastrar Colaboradores" />
+                    <Button onClick={"/"} icon={<MdPeopleOutline size={36} color="white" />} text="Gerenciar Operações" />
+                    <Button onClick={"/"} icon={<PiTreeStructure size={36} color="white" />} text="Gerenciar Departamentos" />
+                </div>
+            </div>
+            <div className="flex gap-6">
+                <div className="flex flex-col gap-6">
+                    {/* AQUI ESTÁ A MUDANÇA: O LINK AGORA APONTA PARA A NOVA TELA */}
+                    <Button onClick={"/rh/processos-burocraticos"} isFulled icon={<VscSync size={36} color="black" />} text="Processos Burocráticos" />
+                    <Button onClick={"/rh/processos-burocraticos"} isDashed icon={<FaPlus size={36} color="white" />} text="Novo Processo" />
+                </div>
+                <DataMetrics department="Recursos Humanos" />
+            </div>
+        </main>
+    )
+}
+
+export default Rh
