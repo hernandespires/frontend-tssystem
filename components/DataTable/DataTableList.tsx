269c590c50a1bebf453b2522c53cc3bfc5e244c1
LuizzB <DevLuizHBarros@gmail.com>
2026-02-17 23:32:50 -0300

    test

--- components/DataTable/DataTableList.tsx
+++ components/DataTable/DataTableList.tsx
@@ -1,8 +1,17 @@
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
-import { Item, ItemActions, ItemContent, ItemDescription, ItemGroup, ItemMedia, ItemSeparator, ItemTitle } from "../ui/item"
+import {
+	Item,
+	ItemActions,
+	ItemContent,
+	ItemDescription,
+	ItemGroup,
+	ItemMedia,
+	ItemSeparator,
+	ItemTitle
+} from "../ui/item"
 import { Button } from "../ui/button"
 import { IoIosArrowForward } from "react-icons/io"
-import { Employee } from "@/types/services/humanResources/employee" 
+import { Employee } from "@/types/services/humanResources/employee"
 import { FaUserFriends } from "react-icons/fa"
 import { findEmployeeById } from "@/services/humanResources/employee"
 import { Fragment } from "react/jsx-runtime"
@@ -11,49 +20,55 @@ import { useContext } from "react"
 import { useRouter } from "next/navigation"
 
 const DataTableList = ({ data }: { data: Employee[] }) => {
-    const { setEmployeeFound } = useContext(FindEmployeeContext)
-    const route = useRouter()
+	const { setEmployeeFound } = useContext(FindEmployeeContext)
+	const route = useRouter()
 
-    const handleShowRegisteredEmployee = async (id: string) => {
-        setEmployeeFound(await findEmployeeById(id))
-        route.push("/rh/cadastro-colaborador")
-    }
+	const handleShowRegisteredEmployee = async (id: string) => {
+		setEmployeeFound(await findEmployeeById(id))
+		route.push("/rh/cadastro-colaborador")
+	}
 
-    return (
-        <ItemGroup className="">
-            { data && data.length > 0 ? (
-                data.map((data) => (
-                    <Fragment key={data.id}>
-                        <Item className="p-2.5 text-default-orange">
-                            <ItemMedia>
-                                <Avatar>
-                                    <AvatarImage src="https://github.com/shadcn.png" />
-                                    <AvatarFallback>
-                                        {data.name.charAt(0)}
-                                    </AvatarFallback>
-                                </Avatar>
-                            </ItemMedia>
-                            <ItemContent className="p-0">
-                                <ItemTitle>
-                                    {data.name} 
-                                </ItemTitle>
-                                <ItemDescription>
-                                    {data.email}
-                                </ItemDescription>
-                            </ItemContent>
-                            <ItemActions>
-                                <Button variant="ghost" size="icon" className="rounded-full" onClick={() => handleShowRegisteredEmployee(data.id)}>
-                                    <IoIosArrowForward color="white" />
-                                </Button>
-                            </ItemActions>
-                            <ItemSeparator className="bg-default-border-color" />
-                        </Item>
-                    </Fragment>
-                ))
-            ) : <div className="flex justify-center items-center h-full flex-col"><FaUserFriends size={150} color="#ffffff2b" /><h1 className="font-bold text-gray-300">Nenhum funcionário cadastrado</h1></div> }
-            {/* : <div className="flex items-center gap-4"><Skeleton className="w-12 h-12 rounded-full" /><div className="space-y-2"><Skeleton className="w-62.5 h-4" /><Skeleton className="w-50 h-4" /></div></div> */}
-        </ItemGroup>
-    )
+	return (
+		<ItemGroup className="">
+			{data && data.length > 0 ? (
+				data.map((data) => (
+					<Fragment key={data.id}>
+						<Item className="p-2.5 text-default-orange">
+							<ItemMedia>
+								<Avatar>
+									<AvatarImage src="https://github.com/shadcn.png" />
+									<AvatarFallback>{data.name.charAt(0)}</AvatarFallback>
+								</Avatar>
+							</ItemMedia>
+							<ItemContent className="p-0">
+								<ItemTitle>{data.name}</ItemTitle>
+								<ItemDescription>{data.email}</ItemDescription>
+							</ItemContent>
+							<ItemActions>
+								<Button
+									variant="ghost"
+									size="icon"
+									className="rounded-full"
+									onClick={() => handleShowRegisteredEmployee(data.id)}
+								>
+									<IoIosArrowForward color="white" />
+								</Button>
+							</ItemActions>
+							<ItemSeparator className="bg-default-border-color" />
+						</Item>
+					</Fragment>
+				))
+			) : (
+				<div className="flex justify-center items-center h-full flex-col">
+					<FaUserFriends size={150} color="#ffffff2b" />
+					<h1 className="font-bold text-gray-300">
+						Nenhum funcionário cadastrado
+					</h1>
+				</div>
+			)}
+			{/* : <div className="flex items-center gap-4"><Skeleton className="w-12 h-12 rounded-full" /><div className="space-y-2"><Skeleton className="w-62.5 h-4" /><Skeleton className="w-50 h-4" /></div></div> */}
+		</ItemGroup>
+	)
 }
 
 export default DataTableList
