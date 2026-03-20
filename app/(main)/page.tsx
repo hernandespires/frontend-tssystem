5e4f665c218daac65793587b630d644eeda07530
LuizzB <devluizhbarros@gmail.com>
2026-03-20 09:27:31 -0300

    fix(routing): redirecionar / para /comercial em vez de /login

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 
 const Home = async () => {    
-    redirect("/login")
+    redirect("/comercial")
 
     // return <main className="px-4 py-7 flex flex-col min-h-screen">test</main>
 }
